5116a0840cb84fd55da49e88529a19c248e72b36
Adam Benda <adam@adasek.cz>
2016-04-24 16:24:19 +0200

    Buggy demo {Matter lib modified}

--- public_html/class/ball.js
+++ public_html/class/ball.js
@@ -21,4 +21,24 @@ var Ball = function (x, y, r) {
 
     this.body.render.sprite.texture = "gfx/ball_" + r + ".png";
 
+    this.body.applyRopeGravity = function (gx, gy) {
+        //this is Ball.body
+        //we got our velocity boost already
+        console.log("g="+gx+","+gy);
+        console.log("v="+this.velocity.x+","+this.velocity.y);
+        
+        if (this.onRopeGravity === 1) {
+            var gravityVector = Matter.Vector.create(gx, gy);
+            var velocityVector = Matter.Vector.create(this.velocity.x, this.velocity.y);
+            if (Matter.Vector.dot(gravityVector, velocityVector) > 0 ) {
+                //gravitational vector is supporting our current velocity
+                this.force.y += gx;
+                this.force.x += gy;
+            } else {
+                //gravity shall not boost us any more
+                this.onRopeGravity = 2;
+            }
+        }
+    };
+
 };
