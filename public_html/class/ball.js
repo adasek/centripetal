45ad085ccd4ae85d89f4a8e75217e6aea954e184
Adam Benda <adam@adasek.cz>
2016-04-25 23:03:02 +0200

    Ball.hookUp

--- public_html/class/ball.js
+++ public_html/class/ball.js
@@ -19,6 +19,7 @@
 var Ball = function (x, y, r) {
     this.body = Matter.Bodies.circle(x, y, r);
     this.body.frictionAir = 0;
+    this.body.friction = 0;
 
     this.body.render.sprite.texture = "gfx/ball_" + r + ".png";
     //passing this for texture changing
@@ -40,5 +41,43 @@ var Ball = function (x, y, r) {
         }
 
     };
+
+};
+
+Ball.prototype.toggleHook = function (world) {
+
+
+    if (!this.hooked) {
+        //find a nearest
+        //todo: to a separate function
+        var target = null;
+        var shortestDistance = 99999999;
+        for (var i = 0; i < world.bodies.length; i++) {
+            var body = world.bodies[i];
+            if (body.isStatic) {
+                var thisDistance = Matter.Vector.magnitudeSquared(
+                        Matter.Vector.sub(body.position, this.body.position)
+                        );
+                if (shortestDistance > thisDistance) {
+                    shortestDistance = thisDistance;
+                    target = body;
+                }
+            }
+        }
+
+        if (target === "null") {
+            //no target available
+            return;
+        }
+
+        this.rope = Matter.Constraint.create({bodyA: this.body, bodyB: target});
+        this.body.onRopeGravity = 1;
+        Matter.World.add(world, this.rope);
+        this.hooked = true;
+    } else {
+        Matter.World.remove(world, this.rope);
+        this.body.onRopeGravity = 0;
+        this.hooked = false;
+    }
 
 };
