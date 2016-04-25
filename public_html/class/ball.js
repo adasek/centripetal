cd20d50d1c5ed7e1bd811723238e68fc7a091a92
Adam Benda <adam@adasek.cz>
2016-04-25 22:43:31 +0200

    Deleting gravity boost

--- public_html/class/ball.js
+++ public_html/class/ball.js
@@ -21,7 +21,8 @@ var Ball = function (x, y, r) {
     this.body.frictionAir = 0;
 
     this.body.render.sprite.texture = "gfx/ball_" + r + ".png";
-
+    //passing this for texture changing
+    this.body.r = r;
 
     /**
      * Disable effect of ordinary gravity and apply my special rope gravity
@@ -38,31 +39,6 @@ var Ball = function (x, y, r) {
             this.force.y -= gy * this.mass;
         }
 
-
-        if (this.onRopeGravity === 1) {
-            var gravityVector = Matter.Vector.create(gx, gy);
-            /*
-             * gravityVector supports our movement if
-             * it is in half plain defined by line with norm=this.velocity
-             * and point this.velocity
-             * ax+by+c=0 with (0,0) present in the line
-             * ax+by<>0 defines half-plains, a = -this.velocity.y, b=this.velocity.x
-             * <=> signum a*gravity.x+b*gravity.y === signum a*this.velocity.x+b*this.velocity.y  
-             */
-            var a = -this.velocity.y;
-            var b = this.velocity.x;
-            if (Math.sign(a * gx + b * gy) === Math.sign(a * this.velocity.x + b * this.velocity.y)) {
-                //gravitational vector is supporting our current velocity
-                this.force.y += gx * this.mass;
-                this.force.x += gy * this.mass;
-            } else {
-                console.log("g=" + gravityVector.x + " " + gravityVector.y);
-                console.log("v=" + this.velocity.x + " " + this.velocity.y);
-                console.log(Matter.Vector.angle(gravityVector, this.velocity));
-                //gravity shall not boost us any more
-                this.onRopeGravity = 2;
-            }
-        }
     };
 
 };
