a3787b5d3c0a8c8e1ba32292b011b11b0ce6474d
Adam Benda <adam@adasek.cz>
2016-05-17 22:06:04 +0200

    Pylon colors

--- public_html/class/pylon.js
+++ public_html/class/pylon.js
@@ -15,10 +15,19 @@
  * @param {number} y - y coordinate 
  * @returns {Ball}
  */
-var Pylon = function (engine, x, y) {
+var Pylon = function (engine, x, y, cnt) {
     this.engine = engine;
     this.body = Matter.Bodies.rectangle(engine.render.options.width * x, engine.render.options.height * y, 10, 10);
     Matter.Body.setStatic(this.body, true);
     this.body.collisionFilter.mask = 2;
 
+    if ((Pylon.cnt++) % 2 === 0) {
+        this.body.render.fillStyle = "#002d56";
+        this.body.render.strokeStyle = "#002d56";
+    } else {
+        this.body.render.fillStyle = "#d59f0f";
+        this.body.render.strokeStyle = "#d59f0f";
+    }
 };
+
+Pylon.cnt = 0;
