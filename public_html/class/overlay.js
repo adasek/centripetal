3f8e89aad88c8e1ce13f7b5898ca536058cf3502
Adam Benda <adam@adasek.cz>
2016-05-17 22:59:30 +0200

    FB share

--- public_html/class/overlay.js
+++ public_html/class/overlay.js
@@ -38,9 +38,31 @@ Overlay.prototype.hide = function () {
 };
 
 Overlay.prototype.showEndScreen = function (gamestate) {
-    this.show("Dospěl jsi s Evelínou ke <strong>skóre " + (Math.round(gamestate.getScore())) +
-            "</strong>" + "<p><a href=\"#\" id=\"againA\">Chceš to zkusit znovu?</a></p>" +
-            "<p>A už jsi evaluoval? Čím více dotazníků, tím lepší bonusy :)</p>");
+    var html = "";
+    html += "<p>Evelína pod tvým vedením získala <strong>skóre " + (Math.round(gamestate.getScore())) + "</strong></p>";
+    html += "<div class=\"fb-share-button\"";
+    html += "data-href=\"http://rotarium.cz/\""
+    html += "data-layout=\"button_count\">"
+    html += "</div>";
+
+    html += "<a href=\"#\" id=\"againA\">Hrát znovu</a>";
+    html += "<hr>";
+    html += "<h2>Ohodnoť kurzy letního semestru a odemkni další level!</h2>";
+    html += "<p>S každými 2.000 odeslaných dotazníků čeká Evelínu nová výzva.</p>";
+    html += "<ul>";
+    html += "<li class=\"unfinished\">&nbsp;4.000  kolomaz</li>";
+    html += "<li class=\"unfinished\">&nbsp;6.000  veletoč</li>";
+    html += "<li class=\"unfinished\">&nbsp;8.000  rotační fokus</li>";
+    html += "<li class=\"unfinished\">10.000  cyklický přenos</li>";
+    html += "<li class=\"unfinished\">12.000  dostředivá pomsta</li>";
+    html += "<li class=\"unfinished\">14.000  oběžná zvůle</li>";
+    html += "<li class=\"unfinished\">16.000  centrální přetížení</li>";
+    html += "<li class=\"unfinished\">18.000  smrtící tečna</li>";
+    html += "<li class=\"unfinished\">20.000  kataklyzmatická orbitální jízda</li>";
+    html += "</ul>";
+
+
+    this.show(html);
 
     document.getElementById('againA').onclick = gamestate.restartAndStart.bind(gamestate);
 };
@@ -48,7 +70,7 @@ Overlay.prototype.showEndScreen = function (gamestate) {
 
 Overlay.prototype.showBeginScreen = function (gamestate) {
     var html = "";
-    html += "<h1>Kolosvist</h1>";
+    html += "<h1>Rotarium</h1>";
     html += "<h2>Evelína v jednom kole</h2>";
     html += "<p>Ovládání: <em>kliknutí nebo mezerník</em></p>";
     html += "<a id=\"enterGameA\">Hrát!</a>";
